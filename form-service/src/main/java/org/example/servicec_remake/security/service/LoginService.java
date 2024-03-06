221a5952570ed013cab7b63fab814e52d1dc455a
mate_morvai <morvai21mate@gmail.com>
2024-03-06 17:16:30 +0100

    codehunter : fixing pipeline, checkstyle

--- form-service/src/main/java/org/example/servicec_remake/security/service/LoginService.java
+++ form-service/src/main/java/org/example/servicec_remake/security/service/LoginService.java
@@ -1,25 +1,18 @@
 package org.example.servicec_remake.security.service;
 
 import lombok.AllArgsConstructor;
-import org.example.servicec_remake.security.mapper.UserMapper;
+import lombok.RequiredArgsConstructor;
 import org.example.servicec_remake.security.model.AuthenticatedUser;
 import org.example.servicec_remake.security.model.User;
-import org.example.servicec_remake.security.repository.UserDocument;
-import org.example.servicec_remake.security.repository.UserRepository;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
 import org.springframework.stereotype.Service;
 
 @Service
-@AllArgsConstructor
-public class LoginService implements UserDetailsService {
-
-    private final UserRepository userRepository;
-    private final UserMapper userMapper;
+@RequiredArgsConstructor
+public class LoginService {
 
     private final AuthenticationManager authenticationManager;
 
@@ -40,13 +33,4 @@ public class LoginService implements UserDetailsService {
                 .token(token)
                 .build();
     }
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        UserDocument userDocument = userRepository.findByUsername(username).orElseThrow(
-                () -> new UsernameNotFoundException(username));
-
-        return userMapper.convertUserDocument(userDocument);
-    }
-
 }
